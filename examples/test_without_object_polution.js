2bfabe4b62bbe9a99bcaf846423fe7192a02c7df
Scott Taylor <scott@railsnewbie.com>
2010-05-15 18:20:07 -0400

    mark ditz issue-6 as wont-fix

--- examples/test_without_object_polution.js
+++ examples/test_without_object_polution.js
@@ -5,7 +5,10 @@ require.paths.unshift(__dirname + "/../lib");
 var sc = require("scriptcheck");
 sc.setup();
 
-sc.run(10000, sc.ForAll({x: sc.Int}, function(params) {
-  var x = params.x;
-  return sc.expectEqual((x * x / x), x);
-}));
+with(sc) {
+  run(10000, ForAll({x: Int}, function(params) {
+    var x = params.x;
+    return expectEqual((x * x / x), x);
+  }));
+}
+
