0a8b63a422047c028e00a3685873f964fb4423b1
Scott Taylor <scott@railsnewbie.com>
2010-05-17 23:37:32 -0400

    update + add examples to the interface

--- examples/test_without_object_polution.js
+++ examples/test_without_object_polution.js
@@ -6,9 +6,21 @@ var sc = require("scriptcheck");
 sc.setup();
 
 with(sc) {
-  run(10000, ForAll({x: Int}, function(params) {
-    var x = params.x;
-    return expectEqual((x * x / x), x);
-  }));
-}
+  describe("a number should equal itself", function() {
+    Int.forAll(function(anInt) {
+      return expectEqual(anInt, anInt);
+    });
+  });
 
+  describe("the square root of a square should be itself", function() {
+    Int.forAll(function(anInt) {
+      return expectEqual(Math.sqrt(anInt * anInt), anInt);
+    });
+  });
+
+  describe("the division of a square by itself should be the number", function() {
+    Int.forAll(function(anInt) {
+      return expectEqual((anInt * anInt / anInt), anInt);
+    });
+  });
+}
