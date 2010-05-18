873d0a3bbc5d0480a5ef151b70a593db4f97d3f5
Scott Taylor <scott@railsnewbie.com>
2010-05-18 01:22:10 -0400

    Update spec/node.js with new one generated by jspec init

--- spec/node.js
+++ spec/node.js
@@ -1,15 +1,10 @@
 
-require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby/1.8.6/gems/jspec-3.3.2/lib', 'lib');
-require('jspec');
-
-require.paths.unshift(__dirname + "/../lib");
-require('unit/spec.helper');
+require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby-1.8.6-p399/gems/jspec-4.3.1/lib', 'lib')
+require('jspec')
+require('unit/spec.helper')
+require('yourlib')
 
 JSpec
-  .exec('spec/scriptcheck/generators/int_spec.js')
-  .exec('spec/scriptcheck/properties/forall_spec.js')
-  .exec('spec/scriptcheck/framework/expect_equal_spec.js')
-  .exec('spec/scriptcheck/reporters/dot_reporter_spec.js')
-  .exec('spec/scriptcheck/scriptcheck_spec.js')
-  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
-  .report();
+  .exec('spec/unit/spec.js')
+  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures', failuresOnly: true })
+  .report()
