61524d8e88b4ee8bfba7cfc27ade2aa438893311
Scott Taylor <scott@railsnewbie.com>
2010-05-20 23:33:22 -0400

    move jspec specs into spec/jspec

---
+++ spec/jspec/node.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+require.paths.unshift(path.join(__dirname, "..", "vendor", "jspec", "lib"));
+require.paths.unshift(path.join(__dirname, "..", "vendor", "jspec_dot_reporter"));
+require.paths.unshift("spec/jspec");
+require.paths.unshift("lib");
+
+require('jspec');
+require('jspec_dot_reporter');
+
+JSpec
+  .exec('spec/jspec/scriptcheck/generators/int_spec.js')
+  .exec('spec/jspec/scriptcheck/properties/forall_spec.js')
+  .exec('spec/jspec/scriptcheck/framework/expect_equal_spec.js')
+  .exec('spec/jspec/scriptcheck/framework/describe_spec.js')
+  .exec('spec/jspec/scriptcheck/framework/runner_spec.js')
+  .exec('spec/jspec/scriptcheck/reporters/dot_reporter_spec.js')
+  .exec('spec/jspec/scriptcheck/scriptcheck_spec.js')
+  .run({ reporter: JSpecDotReporter, fixturePath: 'spec/fixtures', failuresOnly: true })
+  .report();
