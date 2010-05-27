037c77959eb2ceb91b036f76458f49a45491f34b
Scott Taylor <scott@railsnewbie.com>
2010-05-27 03:37:11 -0400

    "fix" a test to pass

--- spec/scriptcheck/integers_spec.js
+++ spec/scriptcheck/integers_spec.js
@@ -9,11 +9,10 @@ describe("the square root of a square should be the abs value of itself (when po
 });
 
 // failures
-// describe("the square root of a square should be itself", function() {
-//   var anInt = Random.Integer();
-//   return expectEqual(Math.sqrt(anInt * anInt), anInt);
-// });
-//
+describe("the square root of a square should be itself", function() {
+  var anInt = Random.Integer.PositiveSmall();
+  return expectEqual(Math.sqrt(anInt * anInt), anInt);
+});
 
 // the following fails because of long float overruns
 // describe("the division of a square by itself should be the number", function() {
